704ba6d3af24cf38e3045fa0bccff7802c72bd00
Multus Trollov <renatgaripov@MacBook-Air-Renat.local>
2015-10-01 22:19:24 +0300

    Урок 6

--- RailStation.rb
+++ RailStation.rb
@@ -1,17 +1,29 @@
 class Train
-def initialize(number, type, vag, work = :work)
+  def initialize(number, type, vag, fuel, work = :work, status = :no)
 	  @vag = vag
+	  @vag = []
 	  @number = number
 	  @type = type
 	  @speed = speed
       @work = work
 	  @speed = 0
-	  @vag = 1
-end
-	
+	  @vag_size = 1
+	  @vag_size = vag.size
+	  @fuel = fuel
+	  @fuel = 0
+	  @status = go
+	  @status = :no
+	  if status == :no
+	  	@speed = 0
+	  end
+	  if speed == 0
+	  	@status = :no
+	  end
+  end
 	def uskoreniye
 		@speed += 10
-		puts "speed = #{@speed}"		
+		@fuel -= 1
+		puts "speed = #{@speed}"
 		if @speed == 110
 			puts "uspokoysya durak!"
 		elsif @speed >= 110
@@ -20,6 +32,8 @@ end
 			@work = :unwork
 			puts "speed = #{@speed}"
 			puts "ti slomal poezd!"
+	    elsif @fuel == 0
+	    	puts "net topliva"
 			# sleep 600
 			# puts "teper vse gotovo! ezd ostorozhno!"
 			# train = work
@@ -38,31 +52,96 @@ end
 		puts "gotovo"
 		@work = :work
 	end
-	def vagon-
-	  if @vag >= 1 && @speed == 0 && @work == :work
-	    @vag -= 1
+	def vagon_minus (vagon)
+	  if @vag_size >= 1 && @speed == 0 && @work == :work
+	    @vag_size -= 1
+	    @vag.delete vagon
 	  	  puts "vag = #{@vag}"
-	elsif @speed > 0
-		puts "ostanovis"
-	elsif @work == :unwork
-		puts "nuzhna pochinka"
+	  elsif @speed > 0
+		  puts "ostanovis"
+	  elsif @work == :unwork
+		  puts "nuzhna pochinka"
 	  elsif @vag <= 1
 		  puts "ty ne posmeesh!"
 	  else
 		  puts "nepoladki"
       end
 	end
-    def vagon+
+    def vagon_plus
     	if @speed == 0 && @work == :work
-    	@vag += 1
-	  puts "vag = #{@vag}"
-	elsif @speed > 0
-		puts "ostanovis"
-	elsif @work == :unwork
-		puts "nuzhna pochinka"
-	  end
+    	  @vag_size += 1
+    	  @vag << vagon
+	      puts "vag = #{@vag}"
+	    elsif @speed > 0
+		  puts "ostanovis"
+	    elsif @work == :unwork
+		  puts "nuzhna pochinka"
+	    end
 	end
 	def vag
 		puts "vag = #{@vag}"
 	end
+	def get_fuel
+		@fuel = 100
+	end
+	def stop
+		go = :no
+	end
+end
+
+class RailStation
+  def initialize (name, trains)
+    @name = name
+    @trains = trains
+    @object = object
+    @object = []
+  end
+
+  def trains
+    puts "sdes #{trains} poezdov"
+    @object.each { |trains| puts "sdes poezda nomer: #{object.number}"}
+  end
+
+  def get(train)
+    @trains += 1
+    @object << train
+  end
+
+  def send(train)
+    if @trains >= 1
+      @trains -= 1
+      @object.delete(train)
+    else
+      puts "net poezdov"
+    end
+  end
+end
+class Rout
+  def initialize (a1stop, stop, laststop, stops)
+  	@a1stop = a1stop
+  	@a1stop = station.new
+  	@stop = stop
+  	@laststop = laststop
+  	@laststop = station.new
+  	@stop = []
+  	@stops = stops
+  	@stops = [@a1stop, @stop, @laststop]
+  	@stops.flatten
+  	@ways = ways
+  	@ways = stops.size - 1
+  	@rout = ways + stops
+  end
+  def watch_rout
+  	puts "#{stops}"
+  end
+  def put(station)
+  	@stop << station
+  end
+  def delete(station)
+  	@stop.delete station
+  end
+end
+class Cargotrain < Train
+end
+class Passengertrain < Train
 end
--- bibika.rb
+++ bibika.rb
@@ -1,12 +1,44 @@
 class Car
+  def initialize
+  	@speed = speed
+  	@status = status
+  	@fuel = fuel
+  	@maxfuel = maxfuel
+  	@maxfuel = 100
+  	@maxspeed = maxspeed
+  	@maxspeed = 500
   def start
-    puts "wrrrrrroooooooommmmmm..."
+    @speed = 100
   end
-
+  def bystrey
+  	@speed += 1
+  	if speed >= maxspeed += 1
+  		puts "nemogu bistrey"
+  	elsif fuel == 0 
+  		puts "net topliva"
+  	end
+  end
+  def bystreyX5
+  	@speed += 5
+  	if speed >= maxspeed += 1
+  		puts "nemogu bistrey"
+  	elsif fuel == 0 
+  		puts "net topliva"
+  	end
+  end
+  def bystreyX10
+  	@speed += 10
+  	if speed >= maxspeed += 1
+  		puts "nemogu bistrey"
+  	elsif fuel == 0 
+  		puts "net topliva"
+  	end
+  end
   def beep
   	puts "beep beep"
   end
-
+  def stop
+  	@speed = 0
   def go(speed, distance)
   	puts "edem"
   	# i = 1000
@@ -19,6 +51,7 @@ class Car
   	puts "..."
   	sleep(time)
   	puts "priyehali"
+  	fuel -= 10
   end
   def kill (people)
   	puts "ah ti podliy utirok!"
@@ -26,7 +59,7 @@ class Car
   		puts "run fores, run!"
   	elsif people == 5
   		puts "run utirok, run!!!"
-  	elsif people >= 5 and <= 10
+  	elsif people >= 5 && people <= 10
   		puts "I HOPE YOU DIE IN A FIRE!!!!!!"
   	else
     puts "illminate watches YOU!"
@@ -35,4 +68,48 @@ class Car
   			puts "DIE!!!!!!!!"
   		end
   	end
+  end
+end
+class SportCar < Car
+  def initilize
+  	@maxfuel = 500
+  	@maxspeed = 5000
+  	super
+  end
+  def start
+    puts "wrrrrrroooooooommmmmm..."
+    super
+  end
+  def bystreyX100
+  	@speed += 100
+  	if speed >= maxspeed += 1
+  		puts "nemogu bistrey"
+  	elsif fuel == 0 
+  		puts "net topliva"
+  	end
+  def bystreyX500
+  	@speed += 500
+  	if speed >= maxspeed += 1
+  		puts "nemogu bistrey"
+  	elsif fuel == 0 
+  		puts "net topliva"
+  	end
+end
+class Track < Car
+  def initialize
+    @items = items
+    @items = []
+    @maxitems = 1000
+    @amountofitems = amountofitems
+    @amountofitems 1000
+    super
+  def load
+  @items < item.new
+  @amountofitems -= 1
+  end
+  def watch
+  	@items.flatten
+  end
+end
+class item
 end
